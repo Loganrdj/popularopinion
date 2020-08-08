9d952b1a3ebffa74c25a5e2c7eef4bce5016543c
Logan Moss <lrdjmoss@gmail.com>
2020-08-07 21:50:44 -0700

    Added working search query for tweets

--- scrape.py
+++ scrape.py
@@ -1,12 +1,18 @@
 import os
 import tweepy
 import config
-# import pandas as pd
+import csv
+import json
+import pandas
+from pandas import DataFrame
+
 consumer_key = config.consumer_key
 consumer_secret = config.consumer_secret
 access_token = config.access_token
 access_token_secret = config.access_token_secret
-userID = "realDonaldTrump"
+# userID = "realDonaldTrump"
+# userID = input("Please input your user ID: ")
+queryTerm = input("What would you like to search for? ")
 
 # -----------------------------------------
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -14,27 +20,6 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 # -----------------------------------------
 
-tweets = api.user_timeline(screen_name = userID, count = 200, include_rts = False, tweet_mode = "extended")
-
-for info in tweets[:3]:
-    print("ID: {}".format(info.id))
-    print(info.created_at)
-    print(info.full_text)
-    print("\n")
-
-all_tweets = []
-all_tweets.extend(tweets)
-oldest_id = tweets[-1].id
-
-while True:
-    tweets = api.user_timeline(screen_name=userID,
-    count=200,
-    include_rts=False,
-    max_id = oldest_id - 1,
-    tweet_mode = "extended")
-    if(len(tweets) == 0):
-        break
-    oldest_id = tweets[-1].id
-    all_tweets.extend(tweets)
-
-    
+tweets = tweepy.Cursor(api.search, q=queryTerm, lang="en").items(1)
+for tweet in tweets:
+    print(tweet.text)
